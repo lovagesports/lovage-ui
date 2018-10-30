a89138197f3d1261b3cc2e3579a64def313c8bdc
Paul Marina <lovagesports@gmail.com>
2018-10-30 23:31:27 +0200

    Added list of fields

--- src/app/domain/field.ts
+++ src/app/domain/field.ts
@@ -1,4 +1,5 @@
 export class Field {
   id: number;
   name: string;
+  location: string;
 }
--- src/app/fields/fields.component.ts
+++ src/app/fields/fields.component.ts
@@ -1,17 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Field } from '../domain/field';
+import { FIELDS } from '../domain/mock-fields';
 
 @Component({
   selector: 'app-fields',
   templateUrl: './fields.component.html',
   styleUrls: ['./fields.component.css']
 })
+
 export class FieldsComponent implements OnInit {
+
+  fields = FIELDS;
 
   field: Field = {
     id: 1,
-    name: 'Balcescu'
+    name: 'Balcescu',
+    location: 'Balcescu'
   };
+
+  selectedField: Field;
+  
+  onSelect(field: Field): void {
+    this.selectedField = field;
+  }
 
   constructor() { }
 
